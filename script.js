3e705765b7d88386cdffb82d67f8074834215c27
MrTibbz2 <155355235+MrTibbz2@users.noreply.github.com>
2024-09-30 12:25:18 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('.animate');
+
+    const options = {
+        root: null,
+        threshold: 0.1,
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('fade-in');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, options);
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
